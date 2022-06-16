9d7a02108a5474c6220b76970f864703ef5cb6fa
Kizzzy <xuhua0174@163.com>
2022-06-16 08:04:24 +0800

    [tencent] * refactor vfs

--- src/main/java/cn/kizzzy/tencent/IdxItem.java
+++ src/main/java/cn/kizzzy/tencent/IdxItem.java
@@ -4,9 +4,9 @@ import cn.kizzzy.helper.ZlibHelper;
 import cn.kizzzy.io.ByteArrayInputStreamReader;
 import cn.kizzzy.io.IFullyReader;
 import cn.kizzzy.io.SliceFullReader;
-import cn.kizzzy.vfs.IStreamable;
+import cn.kizzzy.vfs.IInputStreamGetter;
 
-public class IdxItem implements IStreamable {
+public class IdxItem implements IInputStreamGetter {
     public int pathLength;
     public String path;
     public int reserved01;
@@ -15,25 +15,25 @@ public class IdxItem implements IStreamable {
     public int size;
     
     public String pkg;
-    private IStreamable source;
+    private IInputStreamGetter source;
     
     @Override
-    public IStreamable getSource() {
+    public IInputStreamGetter getSource() {
         return source;
     }
     
     @Override
-    public void setSource(IStreamable source) {
+    public void setSource(IInputStreamGetter source) {
         this.source = source;
     }
     
     @Override
-    public IFullyReader OpenStream() throws Exception {
+    public IFullyReader getInput() throws Exception {
         if (getSource() == null) {
             throw new NullPointerException("source is null");
         }
         
-        IFullyReader reader = new SliceFullReader(source.OpenStream(), offset, size);
+        IFullyReader reader = new SliceFullReader(source.getInput(), offset, size);
         byte[] buffer = reader.readBytes(size);
         buffer = ZlibHelper.uncompress(buffer);
         return new ByteArrayInputStreamReader(buffer);
--- src/main/java/cn/kizzzy/tencent/vfs/pack/IdxPackage.java
+++ src/main/java/cn/kizzzy/tencent/vfs/pack/IdxPackage.java
@@ -1,48 +1,12 @@
 package cn.kizzzy.tencent.vfs.pack;
 
 import cn.kizzzy.tencent.IdxItem;
-import cn.kizzzy.vfs.IFileSaver;
-import cn.kizzzy.vfs.IStreamable;
 import cn.kizzzy.vfs.ITree;
-import cn.kizzzy.vfs.Separator;
-import cn.kizzzy.vfs.pack.AbstractPackage;
-import cn.kizzzy.vfs.streamable.FileStreamable;
-import cn.kizzzy.vfs.tree.Leaf;
+import cn.kizzzy.vfs.pack.LeafPackage;
 
-public class IdxPackage extends AbstractPackage {
+public class IdxPackage extends LeafPackage<IdxItem> {
     
-    public IdxPackage(String root, ITree tree) {
-        super(root, tree);
-    }
-    
-    @Override
-    public boolean exist(String path) {
-        return tree.getLeaf(path) != null;
-    }
-    
-    @Override
-    protected IStreamable getStreamableImpl(String path) {
-        Leaf leaf = tree.getLeaf(path);
-        if (leaf == null || !(leaf.item instanceof IdxItem)) {
-            return null;
-        }
-        
-        IdxItem idxItem = (IdxItem) leaf.item;
-        
-        String fullPath = Separator.FILE_SEPARATOR.combine(root, dealWithPkgName(idxItem.pkg));
-        if (idxItem.getSource() == null) {
-            idxItem.setSource(new FileStreamable(fullPath));
-        }
-        
-        return idxItem;
-    }
-    
-    @Override
-    protected <T> boolean saveImpl(String path, T data, IFileSaver<T> saver) throws Exception {
-        return false;
-    }
-    
-    protected String dealWithPkgName(String pkg) {
-        return pkg;
+    public IdxPackage(String root, ITree tree, String ext) {
+        super(root, tree, IdxItem.class, item -> item.pkg.replace(ext, "pkg"));
     }
 }
